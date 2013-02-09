771aab28de021c5ceca0f334bbef77945f78b2fd
Jill Arnold <arnoldj1@michigan.gov>
2013-02-09 12:25:27 -0500

    Working in some divs to style content

--- single-portfolio.php
+++ single-portfolio.php
@@ -19,8 +19,9 @@ while ( have_posts() ) : the_post(); ?>
 	$portfolio_url = "http://cdm15867.contentdm.oclc.org/cdm/search/collection/".$portfolio_cdmid;
 ?>
 
-<?php if($e404_options['page_titles']) : ?><h2><?php the_title(); ?></h2><?php endif; ?>
+<?php if($e404_options['page_titles']) : ?><div id="Portfolio-title"><h2><?php the_title(); ?></h2></div><?php endif; ?>
 <div id="contentdm-button"><a href="<?php echo $portfolio_url; ?>">View Entire Collection</a></div>
+<br class="clear" />
 <?php if($e404_options['portfolio_item_categories']) : ?>
 			<div class="portfolio-meta">
 				<div class="posted-meta"><span><?php echo e404_get_taxonomy_name('categories'); ?>:</span> <?php the_terms($post->ID, 'portfolio-category', '', ', '); ?></div>
